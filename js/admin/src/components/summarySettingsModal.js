57765fbb842380dfa7cbbb29d01920420410500f
Jordan Schnaidt <jordanschnaidt@gmail.com>
2016-03-09 14:19:35 -0600

    Initial settings modal created.

--- js/admin/src/components/summarySettingsModal.js
+++ js/admin/src/components/summarySettingsModal.js
@@ -1,20 +1,20 @@
 import SettingsModal from 'flarum/components/SettingsModal';
-import Checkbox from 'flarum/components/Checkbox';
 
 export default class SummarySettingsModal extends SettingsModal {
         className() {
             return 'SummarySettingsModal Modal--small';
         }
-        
+
         title() {
             return 'Summaries Settings';
         }
-        
+
         form() {
             return [
-                <div className="Form-group">
-                    <label>Summaries Settings</label>
-                    <input type="checkbox" name
-            ]
+              <div className="Form-group">
+                <label>Summaries Settings</label>
+                <input className="FormControl" bidi={this.setting('flarum-ext-summaries.excerpt_length')}/>
+              </div>
+            ];
         }
 }
