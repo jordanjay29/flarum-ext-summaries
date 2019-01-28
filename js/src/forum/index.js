00a981f691db91a441d827bd3d6d1e35215ab7bd
Christian Lopez <Ralkage@live.com>
2019-01-28 13:17:28 -0500

    Beta 8 compatibility

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,7 +1,7 @@
 import { extend, notificationType } from 'flarum/extend';
 import app from 'flarum/app';
 
-import addSummaryExcerpt from 'jordanjay29/summaries/addSummaryExcerpt';
+import addSummaryExcerpt from './addSummaryExcerpt';
 
 app.initializers.add('jordanjay29-summaries', () => {
   addSummaryExcerpt();
