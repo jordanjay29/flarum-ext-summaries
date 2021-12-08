9cd120fe13dd6ef9e1b11e10567a79a9dd6d6603
Ian Morland <imorland@users.noreply.github.com>
2021-12-08 23:48:34 +0000

    Apply fixes from StyleCI

--- migrations/2021_12_08_000000_add_tags_synopsis_cols.php
+++ migrations/2021_12_08_000000_add_tags_synopsis_cols.php
@@ -1,11 +1,22 @@
 <?php
 
+/*
+ * This file is part of the ianm/synopsis.
+ *
+ * (c) 2020 - 2021 Ian Morland
+ * (c) 2019 Jordan Schnaidt
+ * (c) Toby Zerner <toby.zerner@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 use Flarum\Database\Migration;
 
 return Migration::addColumns(
     'tags',
     [
         'excerpt_length' => ['integer', 'nullable' => true, 'unsigned' => true],
-        'rich_excerpts' => ['boolean', 'nullable' => true]
+        'rich_excerpts'  => ['boolean', 'nullable' => true],
     ]
 );
--- src/Tags/AddTagsAttrs.php
+++ src/Tags/AddTagsAttrs.php
@@ -1,5 +1,16 @@
 <?php
 
+/*
+ * This file is part of the ianm/synopsis.
+ *
+ * (c) 2020 - 2021 Ian Morland
+ * (c) 2019 Jordan Schnaidt
+ * (c) Toby Zerner <toby.zerner@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace IanM\Synopsis\Tags;
 
 use Flarum\Tags\Api\Serializer\TagSerializer;
